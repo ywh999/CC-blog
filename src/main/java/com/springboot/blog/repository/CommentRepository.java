a0e387b6618bdcd0fde0516f471c8e36c8094808
ywhu999 <yaowenhu9@gmail.com>
2022-04-16 11:07:54 -0700

    JPA

--- src/main/java/com/springboot/blog/repository/CommentRepository.java
+++ src/main/java/com/springboot/blog/repository/CommentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//reduce boilerplate code, prevent write dao class and method again and again
+//一开始默认只有findById
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     //带前缀的机制findXXBy,readAXXBy,queryXXBy,countXXBy, getXXBy自动解析的其余部分
     //通过动态代理实现
--- src/main/java/com/springboot/blog/repository/PostRepository.java
+++ src/main/java/com/springboot/blog/repository/PostRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //DAO layer, the JpaRepository do not need @repository annotation, because its father has this annotation
 //and annotation is for class not interface
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+//Spring JPA不是dao provider， 他底层用了Hibernate作为dao provider, spring jpa的主要作用是隐藏persistence layer, 避免重复代码
 }
